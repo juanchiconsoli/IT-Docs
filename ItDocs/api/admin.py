25c26aebf707189f030da99dadd73dd630077915
juanchiconsoli <juanchiconsoli@gmail.com>
2021-06-21 23:43:25 +0200

    Auto add all models to admin

--- ItDocs/api/admin.py
+++ ItDocs/api/admin.py
@@ -1,3 +1,7 @@
+import inspect
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+for name, obj in inspect.getmembers(models):
+    if inspect.isclass(obj):
+        admin.site.register(obj)
